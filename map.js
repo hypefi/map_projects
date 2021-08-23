179ef92d27bf09b46874b3a0d5725b2af4b2367e
hypefi <anasboukharta@gmail.com>
2021-08-23 11:11:42 +0100

    adding saving polygons, 1 - showing properties in console.log ok

--- map.js
+++ map.js
@@ -25,28 +25,28 @@ var editableLayers = new L.FeatureGroup();
 map.addLayer(editableLayers);
 
 var drawPluginOptions = {
-  position: 'topright',
+  position: "topright",
   draw: {
     polygon: {
       allowIntersection: false, // Restricts shapes to simple polygons
       drawError: {
-        color: '#e1e100', // Color the shape will turn when intersects
-        message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
+        color: "#e1e100", // Color the shape will turn when intersects
+        message: "<strong>Oh snap!<strong> you can't draw that!", // Message that will show when intersect
       },
       shapeOptions: {
-        color: '#97009c'
-      }
+        color: "#97009c",
+      },
     },
     // disable toolbar item by setting it to false
     polyline: false,
     circle: false, // Turns off this drawing tool
     rectangle: false,
     marker: false,
-    },
+  },
   edit: {
     featureGroup: editableLayers, //REQUIRED!!
-    remove: false
-  }
+    remove: false,
+  },
 };
 
 // Initialise the draw control and pass it the FeatureGroup of editable layers
@@ -56,17 +56,21 @@ map.addControl(drawControl);
 var editableLayers = new L.FeatureGroup();
 map.addLayer(editableLayers);
 
-map.on('draw:created', function(e) {
+map.on("draw:created", function (e) {
   var type = e.layerType,
     layer = e.layer;
 
-  if (type === 'marker') {
-    layer.bindPopup('A popup!');
+  if (type === "marker") {
+    layer.bindPopup("A popup!");
   }
 
-  editableLayers.addLayer(layer);
-});
 
+  editableLayers.addLayer(layer);
+
+var shapes = getShapes(editableLayers);
+
+console.log(shapes);
+});
 
 var popup = L.popup();
 
@@ -78,3 +82,33 @@ function onMapClick(e) {
 }
 
 map.on("click", onMapClick);
+
+//getshapes function 
+//
+
+
+var getShapes = function(drawnItems) {
+
+    var shapes = [];
+
+    drawnItems.eachLayer(function(layer) {
+
+        // Note: Rectangle extends Polygon. Polygon extends Polyline.
+        // Therefore, all of them are instances of Polyline
+        if (layer instanceof L.Polyline) {
+            shapes.push(layer.getLatLngs())
+        }
+
+        if (layer instanceof L.Circle) {
+            shapes.push([layer.getLatLng()])
+        }
+
+        if (layer instanceof L.Marker) {
+            shapes.push([layer.getLatLng()]);
+        }
+
+    });
+
+    return shapes;
+};
+
