4a39d4b23e04e68e62bd32444f0f4118a2e85db2
hypefi <anasboukharta@gmail.com>
2021-11-11 09:31:58 +0100

    restyling popup / making it a form 1

--- public/layers.js
+++ public/layers.js
@@ -1,3 +1,34 @@
+var popupContent = '<form role="form" id="form" enctype="multipart/form-data" class = "form-horizontal" onsubmit="addMarker()">'+
+          '<div class="form-group">'+
+              '<label class="control-label col-sm-5"><strong>Date: </strong></label>'+
+              '<input type="date" placeholder="Required" id="date" name="date" class="form-control"/>'+ 
+          '</div>'+
+          '<div class="form-group">'+
+              '<label class="control-label col-sm-5"><strong>Gender: </strong></label>'+
+              '<select class="form-control" id="gender" name="gender">'+
+                '<option value="Male">Male</option>'+
+                '<option value="Female">Female</option>'+
+                '<option value="Other">Other</option>'+
+              '</select>'+ 
+          '</div>'+
+          '<div class="form-group">'+
+              '<label class="control-label col-sm-5"><strong>Age: </strong></label>'+
+              '<input type="number" min="0" class="form-control" id="age" name="age">'+ 
+          '</div>'+
+          '<div class="form-group">'+
+              '<label class="control-label col-sm-5"><strong>Description: </strong></label>'+
+              '<textarea class="form-control" rows="6" id="descrip" name="descript">...</textarea>'+
+          '</div>'+
+          '<input style="display: none;" type="text" id="lat" name="lat" value="'+'" />'+
+          '<input style="display: none;" type="text" id="lng" name="lng" value="'+'" />'+
+          '<div class="form-group">'+
+            '<div style="text-align:center;" class="col-xs-4 col-xs-offset-2"><button type="button" class="btn">Cancel</button></div>'+
+            '<div style="text-align:center;" class="col-xs-4"><button type="submit" value="submit" class="btn btn-primary trigger-submit">Submit</button></div>'+
+          '</div>'+
+          '</form>';
+
+
+
 $.getJSON('/maplay',function(result){
     $.each(result, function(i, mlayer){
         console.log("mlayer", mlayer);
@@ -11,12 +42,14 @@ var leaf_layer;
 console.log(layer);
 leaf_layer = L.geoJson(layer, {
       onEachFeature: function (feature, layer) {
-layer.bindPopup('<ul id="myList"><li id="name">'+feature.properties.name+'</li><li>Shade</li><li>Greenery</li></ul>'+'<button onClick="updatefields()">'+'update'+'</button>'+'<button>'+'save'+'</button>', {removable: true, editable: true} );
+// layer.bindPopup('<ul id="myList"><li id="name">'+feature.properties.name+'</li><li>Shade</li><li>Greenery</li></ul>'+'<button onClick="updatefields()">'+'update'+'</button>'+'<button>'+'save'+'</button>', {removable: true, editable: true} );
+
+ layer.bindPopup(popupContent,  {removable: true, editable: true} );
         layer.on({click: layerClickHandler, mouseover: mouseoverfunction});
       }
     });
 
-//leaf_layer.bindPopup(layer.roperties.name);
+  //leaf/_layer.bindPopup(layer.roperties.name);
 //console.log(layer.properties.name);
 //leaf_layer.bindPopup(template);
 leaf_layer.addTo(map);
